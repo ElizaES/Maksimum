64bd99562441e51e32fc7997fd10adc336f53ad6
Eliza Sinkevich <sinkevicheliza@gmail.com>
2020-05-15 14:25:34 +0200

    Add files via upload

---
+++ maximum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie
+{
+    class Maximum
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+           
+            Console.WriteLine("Podaj pierwszą wartość");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj drugą wartość");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj trzecią wartość");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj czwartą wartość");
+            d = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Wartosc maksymalna wynosi " + max_number(max_number(a,b),max_number(c,d)));
+            Console.ReadKey();
+        }
+
+       
+        public static int max_number(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+}
